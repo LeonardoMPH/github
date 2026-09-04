3c926a2e4a5f6e4a05d76d25cd6f23972b50b0d9
gihut <lperalta4@ucol.mx>
2026-09-04 09:24:49 -0600

    subiendo codigo main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("==================================================\n");
+    printf("                     Hola GITHUB                  \n");
+    printf("==================================================\n");
+    printf("Mi nombre es: Leonardo Matias\n");
+    printf("Estoy aprendiendo programacion\n");
+    printf("Este es el primer programa\n");
+    printf("Publicado en github\n");
+    printf("¡Bienvenido a mi repositario\n");
+    return 0;
+}
